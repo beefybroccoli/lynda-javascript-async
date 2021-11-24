4236785b0fb6722dc8046fe3ac763a809d197ad5
beefybroccoli <tedkimemail@yahoo.com>
2021-11-23 19:42:09 -0800

    ...

--- Ex_Files_JavaScript_Async_2021/Ch02/02_03/begin/_scripts/ajax.js
+++ Ex_Files_JavaScript_Async_2021/Ch02/02_03/begin/_scripts/ajax.js
@@ -56,8 +56,8 @@ function tempToF(kelvin) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  // const apiKey = '8017146aaa82ae056dcc283da6da59ee'; // ADD YOUR API KEY BETWEEN THE QUOTES
-  const apiKey = '';
+  const apiKey = '8017146aaa82ae056dcc283da6da59ee'; // ADD YOUR API KEY BETWEEN THE QUOTES
+  // const apiKey = '';
 
   const url =
     'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' +
